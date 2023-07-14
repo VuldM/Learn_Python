8c90f38573086f71191a4c9d83c59fadfba865e0
VoldemarM <aniairina@gmail.com>
2023-07-14 14:40:21 +0300

    change task38

--- Task38.py
+++ Task38.py
@@ -4,15 +4,13 @@
 tel_user = {}
 
 def add_contact():
-    index = 0
-    
     flag = True
     while flag:
          ex_out = input('Добавить да/нет нажимте y or n: ')               
          if ex_out == 'y':
-            index+=1
             name = input('Введите ФИО и номер телефона: ')
-            tel_user.setdefault(index, name)  # если свободен ключ записывает ключ - значение
+            tel = input('Введите номер телефона: ')
+            tel_user.setdefault(name, tel)  # если свободен ключ записывает ключ - значение
             print(tel_user)                              
          elif ex_out =='n':                          
              flag = False 
@@ -21,19 +19,35 @@ def add_contact():
              print('Такой команды нет')
              continue
 def search_contact():
-    flag = True    
-    ex_out = input('Найти контакт да/нет нажимте y or n: ') 
-    if ex_out == 'y':             
-            while flag: 
-               search_name = input('Введите имя или фамилию: ')               
-               keys = tel_user.keys() # Возвращает список ключей из словаря
-               for key in keys:    
-                  tel_user.get(key) # поиск в значениях перебирая ключи
-                  print (tel_user[key])    
-    elif ex_out == 'n':
-             choice_user()    
-    else:
-        print('Такой команды нет')
+    flag = True 
+    while flag:
+        ex_out = input('Найти контакт да/нет нажимте y or n: ') 
+        if ex_out == 'y':
+         search_name = input('Введите имя или фамилию: ')               
+         for i in list(tel_user.keys()):
+            if search_name.lower() in i.lower():
+               print(f'{i} {tel_user.get(i)}')   
+        elif ex_out == 'n':
+              flag = False
+              choice_user()  
+        else:
+              print('Такой команды нет')
+def del_contact():
+    flag = True 
+    while flag:
+        ex_out = input('Удалить контакт да/нет нажимте y or n: ') 
+        if ex_out == 'y':
+         search_name = input('Введите имя или фамилию контакта который хотите удалить: ')               
+         for i in list(tel_user.keys()):
+            if search_name.lower() in i.lower():
+               print(f'{i} {tel_user.get(i)}')  
+               tel_user.pop(i)
+         print(tel_user) 
+        elif ex_out == 'n':
+              flag = False
+              choice_user()  
+        else:
+              print('Такой команды нет')
 def choice_user():
     file = open('tel_contact.txt', 'r', encoding='utf-8')
     lines = file.read().splitlines()
@@ -41,6 +55,7 @@ def choice_user():
     for line in lines: # Проходимся по каждой строчке
        key,value = line.split(': ') # Разделяем каждую строку по двоеточии
        tel_user.update({key:value})	 # Добавляем в словарь
+    print(tel_user)
     flag = True
     choice = input('добавить контакт нажимте "a", найти контакт нажмите "s", удалить контакт нажмита "d"')
     while  flag:        
@@ -52,13 +67,13 @@ def choice_user():
                 search_contact()
                 flag = False
              case 'd':
-                # del_contact()
+                del_contact()
                 flag = False
              case _:
                 print('такой команды нет.')
                 flag = False
     file = open('tel_contact.txt', 'w', encoding='utf-8')
-    for key, value in tel_user.items(): #записываем словать в файл
+    for key, value in tel_user.items(): #записываем словаpь в файл
         file.write('%s: %s\n' % (key, value))
     file.close()
 
